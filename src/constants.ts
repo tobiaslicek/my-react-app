3e3e7c90aedefd7ccf760525b75274cfdb7f7c31
Tobiáš Licek <licek@voatt.cz>
2026-01-20 13:31:26 +0100

    feat: implement routing and page structure for Todo application
    - Added constants for route paths in a new constants.ts file.
    - Integrated react-router-dom for routing in main.tsx, replacing the App component with RouterProvider.
    - Created a router configuration in router.tsx to define application routes and their corresponding components.
    - Added NotFound, TodoDetail, and Todos pages, exporting them from a new index.ts file in the pages directory.
    - Implemented a simple NotFound component to handle 404 errors.

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const ROUTES = {
+  main: "/",
+  todos: '/todos',
+  todoDetail: '/todos/:id',
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
+import { router } from './router.tsx';
 import { theme } from './theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider value={theme}>
-      <App />
+      <RouterProvider router={router} />
+      {/* <App /> */}
     </ChakraProvider>
   </StrictMode>,
 );
