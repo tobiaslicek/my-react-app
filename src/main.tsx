094d17625462033cd3a0af16474464b60dd51cb3
Tobiáš Licek <licek@voatt.cz>
2026-02-06 16:10:57 +0100

    feat: add API integration and configuration for Petstore
    - Introduced .env file to store the API URL for the Petstore.
    - Added orval configuration for generating API client code from OpenAPI spec.
    - Integrated @tanstack/react-query for data fetching and state management.
    - Created queryClient for managing query states and configurations.
    - Implemented API client using ky for making requests to the Petstore API.
    - Added hooks for setting request headers and handling response errors.
    - Generated API types and methods based on OpenAPI specification.
    - Updated package.json and package-lock.json with new dependencies for orval and react-query.

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,16 @@ import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import { router } from './router.tsx';
 import { theme } from './theme.ts';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './queryClient.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider value={theme}>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       {/* <App /> */}
+      </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>,
 );
